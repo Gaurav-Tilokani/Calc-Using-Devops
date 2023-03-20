d3f08b32c00174a4a205a7c418248a070e4d4c2f
Gaurav Tilokani <54092501+Gaurav-Tilokani@users.noreply.github.com>
2023-03-20 18:30:10 +0530

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
     }
 
     public static void main(String[] args) {
-        System.out.println("hii");
+//         System.out.println("hii");
         Calculator calculator = new Calculator();
         Scanner sc = new Scanner(System.in);
 
